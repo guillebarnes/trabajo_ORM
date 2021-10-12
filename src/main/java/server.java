bb79d66377bb56fdcb254411c5622e0c8bec10da
guillebarnes <guillebarnes_95@hotmail.com>
2021-10-12 15:02:59 -0300

    agrego path en el server y algunos test a la bd

--- src/main/java/server.java
+++ src/main/java/server.java
@@ -1,8 +1,12 @@
+import controllers.PersonaController;
+import controllers.UsuarioController;
+import db.dao.UsuarioDAO;
 import spark.Spark;
 
 public class server {
     public static void main(String[] args) {
         Spark.port(9000);
-        Spark.get("/ping", (request, response) -> "pong");
+        Spark.path("/usuario", UsuarioController::definePaths);
+        Spark.path("/persona", PersonaController::definePaths);
     }
 }
--- src/test/java/EntityManagerTest.java
+++ src/test/java/EntityManagerTest.java
@@ -1,19 +1,38 @@
 import db.EntityManagerHelper;
+import db.dao.PersonaDAO;
+import db.dao.UsuarioDAO;
 import entities.Dni;
 import entities.Persona;
+import entities.Usuario;
 import org.junit.Test;
+
+import java.util.List;
 
 public class EntityManagerTest {
 
     @Test
     public void pruebaInsert(){
-        EntityManagerHelper.beginTransaction();
+        PersonaDAO.save(new Persona(new Dni("DNI", 99999999), "Roberto", "Carlos"));
+    }
 
+    @Test
+    public void pruebaUser(){
+        Usuario userPrueba = new Usuario("guillebarnes", "dds_2021", false);
+        userPrueba.setDatos(new Persona(new Dni("DNI", 39340406), "Guillermo", "Barnes"));
+        UsuarioDAO.save(userPrueba);
+    }
 
-        Persona persona = new Persona(new Dni("DNI", 98989899), "Guillermo", "Barnes");
+    @Test
+    public void pruebaUserValido(){
+        System.out.println(UsuarioDAO.usuarioValido("guillebarnes", "dds_2021"));
+    }
+    @Test
+    public void encontrarPersonaPorId(){
+        Persona personaEncontrada = PersonaDAO.findById(2);
+        System.out.println(personaEncontrada.getId() + " " + personaEncontrada.getNombre() + " " + personaEncontrada.getApellido() + " " +personaEncontrada.getDni().getNroDni());
+    }
+    @Test
+    public void pruebaPersonaConDelegado(){
 
-        EntityManagerHelper.getEntityManager().persist(persona);
-
-        EntityManagerHelper.commit();
     }
 }
