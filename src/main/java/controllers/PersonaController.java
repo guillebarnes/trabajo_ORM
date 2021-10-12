119a60b237308333de55c10e917f60e43f7569e3
guillebarnes <guillebarnes_95@hotmail.com>
2021-10-12 14:46:08 -0300

    cambios en el dao y controller de persona

--- src/main/java/controllers/PersonaController.java
+++ src/main/java/controllers/PersonaController.java
@@ -14,16 +14,18 @@ public class PersonaController {
 
     public static String id(Request req, Response res){
         res.type("application/json");
-
         Gson gson = new Gson();
-
         int idRecibido = Integer.parseInt(req.queryParams("id"));
-
         Persona persona = PersonaDAO.findById(idRecibido);
+        if(persona != null){
+            res.status(200);
+            return gson.toJson(persona);
+        }
+        else{
+            res.status(400);
+            return "{\"status\":400, \"msg\":Persona no encontrada. Id invalido}";
+        }
 
 
-        res.status(200);
-
-        return gson.toJson(persona);
     }
 }
--- src/main/java/db/dao/PersonaDAO.java
+++ src/main/java/db/dao/PersonaDAO.java
@@ -3,6 +3,10 @@ package db.dao;
 import db.EntityManagerHelper;
 import entities.Persona;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class PersonaDAO {
     public static void save(Persona persona){
         EntityManagerHelper.beginTransaction();
@@ -13,5 +17,10 @@ public class PersonaDAO {
     }
     public static Persona findById(int id){
         return EntityManagerHelper.getEntityManager().find(Persona.class, id);
+    }
+    public static void findAll(){
+        EntityManagerHelper.getEntityManager().createQuery("SELECT id, 'persona_nombre', 'persona_apellido' FROM persona").getResultList();
+
+        //debe retornar la lista de todas las personas registradas.
     }
 }
