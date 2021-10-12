e725644d7a3639d7b6d4258e0314f9fd93702fa1
guillebarnes <guillebarnes_95@hotmail.com>
2021-10-12 17:26:31 -0300

    metodo find by dni

--- src/main/java/db/dao/PersonaDAO.java
+++ src/main/java/db/dao/PersonaDAO.java
@@ -18,9 +18,15 @@ public class PersonaDAO {
     public static Persona findById(int id){
         return EntityManagerHelper.getEntityManager().find(Persona.class, id);
     }
+
     public static void findAll(){
         EntityManagerHelper.getEntityManager().createQuery("SELECT id, 'persona_nombre', 'persona_apellido' FROM persona").getResultList();
 
         //debe retornar la lista de todas las personas registradas.
+    }
+
+    public static Long findByDni(int dni){
+        String query = "SELECT COUNT(*) FROM persona p INNER JOIN dni d ON p.persona_dni = d.id WHERE d.dni_nro = " + dni;
+        return (Long) EntityManagerHelper.getEntityManager().createNativeQuery(query).getSingleResult();
     }
 }
