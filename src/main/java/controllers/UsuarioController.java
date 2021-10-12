65b145ff2977e2daf3f8e19396f83ab042c1b388
guillebarnes <guillebarnes_95@hotmail.com>
2021-10-12 17:27:08 -0300

    agrego el post de usuario (revisar)

--- src/main/java/controllers/UsuarioController.java
+++ src/main/java/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import db.dao.PersonaDAO;
 import db.dao.UsuarioDAO;
+import entities.Usuario;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
@@ -8,6 +10,7 @@ import spark.Spark;
 public class UsuarioController {
     public static void definePaths(){
         Spark.get("/check", UsuarioController::check);
+        Spark.post("/registrar", UsuarioController::registrar);
     }
 
     public static String check(Request req, Response res){
@@ -26,6 +29,27 @@ public class UsuarioController {
         else{
             res.status(200);
             return "{\"status\":200, \"msg\":Usuario o contraseña incorrecta}";
+        }
+    }
+    public static String registrar(Request req, Response res){
+        res.type("application/json");
+
+        String dni = req.queryParams("dni");
+        Long respuesta = PersonaDAO.findByDni(Integer.parseInt(dni));
+        if(respuesta == 1){
+            res.status(200);
+            UsuarioDAO.save(new Usuario(req.queryParams("user"), req.queryParams("password"), false));
+            return "{\"status\":200, \"msg\":Registro realizado con éxito}";
+        }
+        else {
+            if(respuesta != 1){
+                res.status(200);
+                return "{\"status\":200, \"msg\":Usted no se encuentra empadronado y no puede registrarse}";
+            }
+            else{
+                res.status(400);
+                return "{\"status\":400, \"msg\":Error en el ingreso de datos}";
+            }
         }
     }
 }
