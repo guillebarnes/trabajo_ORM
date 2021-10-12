c8197f1391c27bb0c240578e9195507c57a2b237
guillebarnes <guillebarnes_95@hotmail.com>
2021-10-12 17:27:41 -0300

    cambio de puerto a spark

--- src/main/java/server.java
+++ src/main/java/server.java
@@ -5,7 +5,7 @@ import spark.Spark;
 
 public class server {
     public static void main(String[] args) {
-        Spark.port(9000);
+        Spark.port(9090);
         Spark.path("/usuario", UsuarioController::definePaths);
         Spark.path("/persona", PersonaController::definePaths);
     }
