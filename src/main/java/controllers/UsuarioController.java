97b1c9f9b2c6f152d843ac0825f0ef742aa1b885
guillebarnes <guillebarnes_95@hotmail.com>
2021-10-14 11:17:39 -0300

    agrego post al controlador de usuario

--- src/main/java/controllers/UsuarioController.java
+++ src/main/java/controllers/UsuarioController.java
@@ -2,18 +2,19 @@ package controllers;
 
 import db.dao.PersonaDAO;
 import db.dao.UsuarioDAO;
+import entities.Persona;
 import entities.Usuario;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
 
 public class UsuarioController {
-    public static void definePaths(){
+    public static void definePaths() {
         Spark.get("/check", UsuarioController::check);
         Spark.post("/registrar", UsuarioController::registrar);
     }
 
-    public static String check(Request req, Response res){
+    public static String check(Request req, Response res) {
         res.type("application/json");
 
         String usuario = req.queryParams("user");
@@ -21,35 +22,34 @@ public class UsuarioController {
 
         Long respuesta = UsuarioDAO.usuarioValido(usuario, contraseña);
 
-        if(respuesta == 1){
+        if (respuesta == 1) {
             res.status(200);
             return "{\"status\":200, \"msg\":Sesion iniciada correctamente}";
-        }
-
-        else{
+        } else {
             res.status(200);
             return "{\"status\":200, \"msg\":Usuario o contraseña incorrecta}";
         }
     }
-    public static String registrar(Request req, Response res){
-        res.type("application/json");
 
-        String dni = req.queryParams("dni");
-        Long respuesta = PersonaDAO.findByDni(Integer.parseInt(dni));
-        if(respuesta == 1){
-            res.status(200);
-            UsuarioDAO.save(new Usuario(req.queryParams("user"), req.queryParams("password"), false));
-            return "{\"status\":200, \"msg\":Registro realizado con éxito}";
-        }
-        else {
-            if(respuesta != 1){
-                res.status(200);
-                return "{\"status\":200, \"msg\":Usted no se encuentra empadronado y no puede registrarse}";
-            }
+    public static String registrar(Request req, Response res) {
+            Persona persona = PersonaDAO.findByDni(Integer.parseInt(req.queryParams("dni")));
+
+            Long respuesta = UsuarioDAO.usuarioValido(req.queryParams("user"), req.queryParams("password"));
+            if(respuesta == 1)
+                return "{\"status\":200, \"msg\":Ya existe usuario con ese nombre}";
             else{
-                res.status(400);
-                return "{\"status\":400, \"msg\":Error en el ingreso de datos}";
+                if(persona != null) {
+                    Usuario usuarioNuevo = new Usuario();
+                    usuarioNuevo.setUsuario(req.queryParams("user"));
+                    usuarioNuevo.setContrasena(req.queryParams("password"));
+                    usuarioNuevo.setAdministrador(false);
+                    usuarioNuevo.setDatos(persona);
+                    UsuarioDAO.save(usuarioNuevo);
+                    return "{\"status\":200, \"msg\":Usuario creado correctamente}";
+                }
+                else
+                    return "{\"status\":200, \"msg\":Usted no se puede registrar porque no se encuentra empadronado con ese dni}";
+
             }
         }
-    }
 }
